419e97d0c9f131255e6d1532c3a48f09dfd51133
Hokuten85 <kellygrantleanna@gmail.com>
2023-04-27 00:05:53 -0500

    boost xp rings

--- modules/ivalice/lua/era_exp_rings.lua
+++ modules/ivalice/lua/era_exp_rings.lua
@@ -6,19 +6,19 @@ require("scripts/globals/status")
 local m = Module:new("era_exp_rings")
 
 m:addOverride("xi.globals.items.chariot_band.onItemUse", function(target)
-  target:addStatusEffect(xi.effect.DEDICATION, 75, 0, 5400, 0, 500)
+  target:addStatusEffect(xi.effect.DEDICATION, 75, 0, 5400, 0, 2500)
 end)
 
 m:addOverride("xi.globals.items.empress_band.onItemUse", function(target)
-  target:addStatusEffect(xi.effect.DEDICATION, 50, 0, 10800, 0, 1000)
+  target:addStatusEffect(xi.effect.DEDICATION, 50, 0, 10800, 0, 5000)
 end)
 
 m:addOverride("xi.globals.items.emperor_band.onItemUse", function(target)
-  target:addStatusEffect(xi.effect.DEDICATION, 50, 0, 12600, 0, 2000)
+  target:addStatusEffect(xi.effect.DEDICATION, 50, 0, 12600, 0, 10000)
 end)
 
 m:addOverride("xi.globals.items.anniversary_ring.onItemUse", function(target)
-  target:addStatusEffect(xi.effect.DEDICATION, 100, 0, 43200, 0, 3000)
+  target:addStatusEffect(xi.effect.DEDICATION, 100, 0, 43200, 0, 30000)
 end)
 
 return m
