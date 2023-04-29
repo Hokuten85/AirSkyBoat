fd8bb664a2655046b2f92c2a15980701b509f879
Hokuten85 <kellygrantleanna@gmail.com>
2023-04-29 16:48:23 -0500

    adjustments to start scripts

--- START/START_SEARCH.cmd
+++ START/START_SEARCH.cmd
@@ -3,7 +3,7 @@ For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 cd ..
 :onCrash
 echo [%date% %time%] Restarting SEARCH Server...
-REM xi_search.exe --log "Z:\FFIvalice\Process Logs\search_%mydate%.log"
-REM xi_search.exe
+xi_search.exe --log "Z:\FFIvalice\Process Logs\search_%mydate%.log"
+REM ../xi_search.exe
 echo Server was stopped or crashed!
 GOTO onCrash
--- START/START_WORLD.cmd
+++ START/START_WORLD.cmd
@@ -3,7 +3,7 @@ For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 cd ..
 :onCrash
 echo [%date% %time%] Restarting WORLD Server...
-REM xi_world.exe --log "Z:\FFIvalice\Process Logs\world_%mydate%.log"
+xi_world.exe --log "Z:\FFIvalice\Process Logs\world_%mydate%.log"
 REM xi_world.exe
 echo Server was stopped or crashed!
 GOTO onCrash
