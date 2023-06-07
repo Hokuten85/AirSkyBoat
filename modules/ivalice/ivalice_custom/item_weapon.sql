5524ce1959a8c8928fc9cb4be9a338d761ab8cb6
Hokuten85 <kellygrantleanna@gmail.com>
2023-06-07 00:50:25 -0500

    semi-colons >_<

--- modules/ivalice/ivalice_custom/item_weapon.sql
+++ modules/ivalice/ivalice_custom/item_weapon.sql
@@ -6,8 +6,7 @@ SET iw.delay = iw.delay * 0.6, iw.dmg = iw.dmg * 0.8
 WHERE jobs = 65536
 AND slot = 4;
 
-UPDATE item_weapon SET dmg = 19 WHERE itemid in (18716) -- marss_hexagun_+1 make sure it has dmg+1
-UPDATE item_weapon SET dmg = 40 WHERE itemid in (18987,19007) -- death_penalty keep dmg the same
-
+UPDATE item_weapon SET dmg = 19 WHERE itemid in (18716); -- marss_hexagun_+1 make sure it has dmg+1
+UPDATE item_weapon SET dmg = 40 WHERE itemid in (18987,19007); -- death_penalty keep dmg the same
 
 UPDATE item_weapon SET delay = 100 WHERE itemid in (17343,19229,18713,18235); -- bronze_bullet,tin_bullet,copper_bullet,corsair_bullet
