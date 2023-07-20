75313fa047a7a226bbfd97a3000a91bba9c4d8e2
Hokuten85 <kellygrantleanna@gmail.com>
2023-07-20 17:23:09 -0500

    trying to fix sunbreeze festival

--- scripts/zones/West_Ronfaure/Zone.lua
+++ scripts/zones/West_Ronfaure/Zone.lua
@@ -6,6 +6,7 @@ require('scripts/quests/i_can_hear_a_rainbow')
 require('scripts/globals/chocobo_digging')
 require('scripts/globals/conquest')
 require('scripts/globals/missions')
+require('scripts/globals/events/sunbreeze_festival')
 -----------------------------------
 local zoneObject = {}
 
