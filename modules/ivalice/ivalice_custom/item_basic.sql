ed97680735ac8cd01ae359824d6b32982491e2a5
Hokuten85 <kellygrantleanna@gmail.com>
2023-07-27 23:23:51 -0500

    modify nin spells, correct name of toolbags to be used with lua files

--- modules/ivalice/ivalice_custom/item_basic.sql
+++ modules/ivalice/ivalice_custom/item_basic.sql
@@ -25,3 +25,14 @@ WHERE itemid IN (13202,13054,13952,17108);
 
 UPDATE item_basic SET stackSize = 99, aH = 51 WHERE itemid IN (4305,4316,4479,5134,5135,5534); -- 'apkallufa', 'armored_pisces', 'bhefhel_marlin', 'mola_mola', 'rhinochimera', 'ryugu_titan'
 UPDATE item_basic SET aH = 49 WHERE itemid IN (2971,2972,2973,5867,5868,5869); -- Shikanofuda, Chonofuda, Inoshishinofuda
+
+UPDATE item_basic SET name = 'toolbag_soshi' WHERE name = 'toolbag_(soshi)';
+UPDATE item_basic SET name = 'toolbag_kabenro' WHERE name = 'toolbag_(kabenro)';
+UPDATE item_basic SET name = 'toolbag_jinko' WHERE name = 'toolbag_(jinko)';
+UPDATE item_basic SET name = 'toolbag_jinko' WHERE name = 'toolbag_(ryuno)';
+UPDATE item_basic SET name = 'toolbag_mokujin' WHERE name = 'toolbag_(mokujin)';
+UPDATE item_basic SET name = 'toolbag_inoshishinofuda' WHERE name = 'toolbag_(inoshishinofuda)';
+UPDATE item_basic SET name = 'toolbag_shikanofuda' WHERE name = 'toolbag_(shikanofuda)';
+UPDATE item_basic SET name = 'toolbag_chonofuda' WHERE name = 'toolbag_(chonofuda)';
+UPDATE item_basic SET name = 'toolbag_ranka' WHERE name = 'toolbag_(ranka)';
+UPDATE item_basic SET name = 'toolbag_furu' WHERE name = 'toolbag_(furu)';
