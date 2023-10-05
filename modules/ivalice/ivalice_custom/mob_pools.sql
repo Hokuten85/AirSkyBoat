8230e7e25b46d5c949d6b1371d162bd091e3d1c7
Hokuten85 <kellygrantleanna@gmail.com>
2023-10-04 20:18:31 -0500

    make bat camp in zeruhn mines

--- modules/ivalice/ivalice_custom/mob_pools.sql
+++ modules/ivalice/ivalice_custom/mob_pools.sql
@@ -1,2 +1,3 @@
-UPDATE  mob_pools SET true_detection = 0 where poolid = 6377; -- fuck Poroggo_Excavator being true sight and blocking access to coffer mobs
-UPDATE mob_pools set links = 0 WHERE poolid = 2580; -- make marsh murre in cadaerva mire not link
+UPDATE mob_pools SET true_detection = 0 where poolid = 6377; -- fuck Poroggo_Excavator being true sight and blocking access to coffer mobs
+UPDATE mob_pools SET links = 0 WHERE poolid = 2580; -- make marsh murre in cadaerva mire not link
+UPDATE mob_pools SET links = 0 WHERE poolid = 6469; -- Colliery_Bat no link				
