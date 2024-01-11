073dfc857c1beb341996624df9f271c4a275837e
Kelly <kellygrantleanna@gmail.com>
2024-01-10 23:06:14 -0600

    derped footwork

--- scripts/globals/effects/footwork.lua
+++ scripts/globals/effects/footwork.lua
@@ -8,7 +8,6 @@ local effectObject = {}
 effectObject.onEffectGain = function(target, effect)
     target:addMod(xi.mod.ATTP, 10)
 	target:addMod(xi.mod.KICK_ATTACK_RATE, 20)
-	KICK_ATTACK_RATE
 end
 
 effectObject.onEffectTick = function(target, effect)
